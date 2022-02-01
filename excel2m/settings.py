675324f65e5fe2fbfdccae6ae7f24384ac167d52
Pankaj Singh Rana <pankajsinghrana2003@gmail.com>
2022-02-01 20:08:08 +0530

    Thisrd commit changing the setting.py file and python verson in procfile

--- excel2m/settings.py
+++ excel2m/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e6o6yt)2$l#-fh1_4kz213_s=&zeme#kcv+-^py2b8-&m(xcx7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
